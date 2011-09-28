2d1e2d517588429900afb9a3e52e965bd2ce6521
vkevroletin <kevroletin@gmail.com>
2011-09-28 02:18:16 +0000

    Изображения при отрисовке в таблицах не растягиваются.

--- picturedelegate.cpp
+++ picturedelegate.cpp
@@ -13,7 +13,21 @@ void PictureDelegate::paint(QPainter *painter, const QStyleOptionViewItem &optio
     if (colomnsToDraw.contains(index.column())) {
         QImage image;
         image.loadFromData(index.data().toByteArray(), "PNG");
-        painter->drawImage(option.rect, image);
+        double sy = double(option.rect.height()) / double(image.height());
+        double sx = double(option.rect.width()) / double(image.width());
+        QRect drawRect = option.rect;
+        if (sx > sy) {
+            double nx = double(image.width()) * sy;
+            double dx = (drawRect.width() - nx) / 2;
+            drawRect.setLeft(drawRect.left() + dx);
+            drawRect.setRight(drawRect.right() - dx);
+        } else {
+            double ny = double(image.height()) * sx;
+            double dy = (drawRect.height() - ny) / 2;
+            drawRect.setTop(drawRect.top() + dy);
+            drawRect.setBottom(drawRect.bottom() - dy);
+        }
+        painter->drawImage(drawRect, image);
     } else {
         QSqlRelationalDelegate::paint(painter, option, index);
     }
